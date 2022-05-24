a32bcbd8583dd3bd2643a28ea6e8372c5f3e2ad7
Marsel <chelovek-gorod@yandex.by>
2022-05-24 14:51:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -550,7 +550,7 @@ animate();
 // 'ws://localhost:6789'
 // 'ws://192.168.100.51:6789'
 // 'ws://192.168.0.122:6789'
-const socketURL = 'ws://localhost:6789';
+const socketURL = 'wss://mars-game-server.herokuapp.com';
 let SOCKET;
 
 // user key generator
